39d35edf769aa5a216e0fe158730920599047544
ValdsonSilva <valdsonmacedo15@gmail.com>
2025-12-20 11:41:28 -0300

    "hotfix: env do cloudinary"

--- src/core/config/cloudinary.ts
+++ src/core/config/cloudinary.ts
@@ -2,9 +2,9 @@
 import { v2 as cloudinary, UploadApiOptions, UploadApiResponse } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET!,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
 });
 
